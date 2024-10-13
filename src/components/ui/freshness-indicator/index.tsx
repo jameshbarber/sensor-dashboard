662f388535944f3e7f0866145ab406313160f1db
James Hilton-Barber <jameshiltonbarber@gmail.com>
2024-10-13 23:48:08 +0200

    Update index.tsx

--- src/components/ui/freshness-indicator/index.tsx
+++ src/components/ui/freshness-indicator/index.tsx
@@ -2,6 +2,11 @@ import { DateTime } from "luxon"
 
 const FreshnessIndicator = ({ date }: { date: number }) => {
 
+    if (!date) return <div className="flex items-center space-x-2">
+        <div className={`w-2 h-2 rounded-full bg-gray-200`}></div>
+        <div>No data</div>
+    </div>
+
     const dt = DateTime.fromMillis(date ?? 0)
     const now = DateTime.now()
 
